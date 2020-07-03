3d84ed0a356482df0d0fea1ed0a5e1cdab04a119
gshong <hgs33hgs@gmail.com>
2020-07-04 01:08:07 +0900

    c# 파일 추가

---
+++ fibonacci/c#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static long fibonacci(long value)
+        {
+            if (value == 0 || value == 1)
+            {
+                return 1;
+            }
+            return fibonacci(value - 1) + fibonacci(value - 2);
+        }
+
+        public static void Main(string[] args)
+        {
+            int fibValue = 45;
+            Console.WriteLine("fibValue: " + fibValue);
+            
+            for (int i = 0; i < 5; i++)
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                long result = fibonacci(fibValue);
+                sw.Stop();
+                Console.WriteLine("result: " + result);
+                Console.WriteLine("time:" + sw.ElapsedMilliseconds.ToString() + "ms");
+            }
+        }
+    }
+}
