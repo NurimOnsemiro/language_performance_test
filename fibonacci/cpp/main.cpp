03cc69a6e008e10b72d98d273ad6238f2c52a8e2
gshong <hgs33hgs@gmail.com>
2020-07-20 21:24:54 +0900

    싱글스레드, 멀티스레드 추가

--- fibonacci/cpp/main.cpp
+++ fibonacci/cpp/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "HourMeter.h"
+#include <future>
+#include "hourmeter.h"
 using namespace std;
 
 long long fibonacci(long long value) {
@@ -9,18 +10,49 @@ long long fibonacci(long long value) {
     return fibonacci(value - 1) + fibonacci(value - 2);
 };
 
+void runSingleThread() {
+    long long fibValue;
+    while (true) {
+        printf("Enter the Fibonacci number to calculate.\nNumber:");
+        scanf("%lld", &fibValue);
+        printf("Fibonacci number: %lld\n", fibValue);
+        HourMeter hm;
+        hm.startMeasure();
+        long long result = fibonacci(fibValue);
+        hm.endMeasure();
+        printf("value: %lld\n", result);
+    }
+}
+
+void runMultiThread() {
+    long long fibValue;
+    const int numCores = static_cast<int>(std::thread::hardware_concurrency());
+    future<long long> thr[32];
+    while (true) {
+        printf("Enter the Fibonacci number to calculate.\nNumber:");
+        scanf("%lld", &fibValue);
+        printf("Fibonacci number: %lld\n", fibValue);
+
+        HourMeter hm;
+        hm.startMeasure();
+
+        for (int i = 0; i < numCores; i++) {
+            thr[i] = async(launch::async, fibonacci, fibValue);
+        }
+
+        for (int i = 0; i < numCores; i++) {
+            thr[i].get();
+        }
+
+        hm.endMeasure();
+    }
+}
+
 int main() {
-    printf("START\n");
+    runSingleThread();
+    //runMultiThread();
 
-    long long fibValue{ 45 };
-    HourMeter hm;
-    hm.startMeasure();
-    long long result = fibonacci(fibValue);
-    hm.endMeasure();
-
-    printf("value: %lld\n", result);
-
-    printf("END\n");
+    printf("Exit\n");
 
     return 0;
 }
