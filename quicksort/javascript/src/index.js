5ce6a06c5b04870e1cf1a57f03908ee31ed25923
gshong <hgs33hgs@gmail.com>
2021-05-01 11:17:41 +0900

    퀵정렬 구현

--- quicksort/javascript/src/index.js
+++ quicksort/javascript/src/index.js
@@ -9,9 +9,82 @@ function generateRandomArray(numData){
 }
 
 function quickSort(arr){
-    let firstLength = arr.length;
-    let low = 0, mid = Math.round(firstLength / 2), high = firstLength - 1;
-    let pivot = Math.round((arr[low] + arr[mid] + arr[high]) / 3);
+    let queue = [];
+    queue.push(0, arr.length - 1);
+
+    while(queue.length !== 0){
+        let low = queue.shift();
+        let high = queue.shift();
+        //console.log(`low : ${low}, high : ${high}`);
+        //INFO: 하나밖에 없으면 정렬 불필요
+        if(low === high) continue;
+        let arrLength = high - low + 1;
+        let pivot = low;
+        let mid = low;
+
+        if(arrLength > 2) {
+            //INFO: 2개 이상인 경우
+            let tmpMid = Math.round((low+high) / 2);
+            //INFO: low, mid, high 중 중간값을 정해야 함
+            let tmpArr = [low, tmpMid, high];
+            tmpArr.sort((a,b) => {return arr[a]-arr[b]});
+            pivot = tmpArr[1];
+        } else {
+            //INFO: 2개 밖에 없는 경우
+            if(arr[low] > arr[high]){
+                let tmp = arr[low];
+                arr[low] = arr[high];
+                arr[high] = tmp;
+            }
+            continue;
+        }
+
+        //INFO: 최소 3개는 있다고 확정할 수 있음
+
+        //INFO: pivot을 가장 왼쪽으로 이동
+        {
+            let tmp = arr[low];
+            arr[low] = arr[pivot];
+            arr[pivot] = tmp;
+            pivot = low;
+        }
+
+        let i = low+1, j=high;
+        while(true){
+            while(i < j && arr[i] <= arr[pivot]){
+                i++;
+            }
+            while(j > i && arr[j] > arr[pivot]){
+                j--;
+            }
+            if(i >= j){
+                //INFO: 더이상 교환할 것이 없다
+                if(i > low){
+                    //INFO: 하나라도 증가했으면 교환
+                    let tmp = arr[i-1];
+                    arr[i-1] = arr[pivot];
+                    arr[pivot] = tmp;
+                    mid = i-1;
+                } else {
+                    //INFO: 피봇보다 큰 값밖에 없으면 피봇이 중간값이 된다
+                    mid = pivot;
+                }
+                break;
+            }
+
+            {
+                let tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+
+        //INFO: low~mid 까지 정렬하고, mid+1~high까지 정렬해야 한다
+        queue.push(low);
+        queue.push(mid);
+        queue.push(mid+1);
+        queue.push(high);
+    }
 }
 
 function main(){
@@ -22,7 +95,6 @@ function main(){
         console.time('sort');
         quickSort(arr);
         console.timeEnd('sort');
-        console.log(arr[0], arr[numData - 1]);
     }
 }
 main();
